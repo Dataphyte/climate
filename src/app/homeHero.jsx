4d68a9108447b4dce0f8c620de9ae67835a9e008
Emmanuel Alawode <alawodeemmanuel2@gmail.com>
2022-10-30 14:57:08 +0100

    testing flourish

--- src/app/homeHero.jsx
+++ src/app/homeHero.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import HomeNavBar from './homeNavBar';
+import HomeNavBar from './homeNavBar.client';
 import WiredIcon from '../components/lib/wiredIcon';
 import { topography } from '../assets/svg/topography.js';
 
